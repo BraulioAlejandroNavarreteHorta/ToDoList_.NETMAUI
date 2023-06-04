e393868e64ecab32ce739d47de57da54ad5b7c96
BraulioAlejandroNavarreteHorta <bnh74570@lasallebajio.edu.mx>
2023-06-03 22:36:28 -0600

    Se agregó el proyecto

---
+++ TDMPW_412_3P_PR02/TDMPW_412_3P_PR02/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+//using static Android.Content.ClipData;
+
+namespace TDMPW_412_3P_PR02;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+    DateTime fecha = DateTime.Now;
+    string pendiente = "";
+    string estado = "";
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private ObservableCollection<Tarea> toDoList;
+
+    public ObservableCollection<Tarea> ToDoList
+    {
+        get { return toDoList; }
+        set
+        {
+            toDoList = value;
+            OnPropertyChanged(nameof(ToDoList));
+        }
+    }
+
+
+    public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+        toDoList = new ObservableCollection<Tarea>();
+        listaTareas.ItemsSource = ToDoList;
+        
+        
+    }
+
+    void btnAgregar_Clicked(System.Object sender, System.EventArgs e)
+    {
+        pendiente = txtTarea.Text;
+        if (!string.IsNullOrEmpty(pendiente))
+        {
+
+            estado = "PENDIENTE";
+            Tarea homework = new Tarea(pendiente, estado, fecha.ToString());
+            ToDoList.Add(homework);
+            listaTareas.ItemsSource = toDoList;
+            txtTarea.Text = "";
+        }
+        else {
+            
+            ShowAlert("¡Cuidado!", "No puedes agregar una tarea vacía");
+        }
+    }
+
+    void btnOk_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Button btn = (Button)sender;
+
+        var item = btn.BindingContext as Tarea;
+
+        if (item != null)
+        {
+            item.status = "COMPLETADO";
+            item.IsTachado = true;
+        }
+        
+    }
+
+    void btnEliminar_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Button btn = (Button)sender;
+
+        var item = btn.BindingContext as Tarea;
+
+        if (item != null)
+        {
+            ToDoList.Remove(item);
+        }
+    }
+
+    private async void ShowAlert(string title, string message)
+    {
+        await Application.Current.MainPage.DisplayAlert(title, message, "Aceptar");
+        
+    }
+
+}
+
+
---
+++ TDMPW_412_3P_PR02/TDMPW_412_3P_PR02/Tarea.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TDMPW_412_3P_PR02
+{
+	public class Tarea: INotifyPropertyChanged
+    {
+        private string _status;
+        private string _tarea;
+        private string _fecha;
+        private bool _isTachado;
+
+        public bool IsTachado
+        {
+            get { return _isTachado; }
+            set
+            {
+                if (_isTachado != value)
+                {
+                    _isTachado = value;
+                    OnPropertyChanged("IsTachado");
+                }
+            }
+        }
+
+        public string status
+        {
+            get { return _status; }
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged("status");
+                }
+            }
+        }
+
+        public string tarea
+        {
+            get { return _tarea; }
+            set
+            {
+                if (_tarea != value)
+                {
+                    _tarea = value;
+                    OnPropertyChanged("tarea");
+                }
+            }
+        }
+
+        public string fecha
+        {
+            get { return _fecha; }
+            set
+            {
+                if (_fecha != value)
+                {
+                    _fecha = value;
+                    OnPropertyChanged("fecha");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public Tarea(string tarea, string status, string fecha)
+		{
+			this.tarea = tarea;
+			this.status = status;
+			this.fecha = fecha;
+		}
+	}
+}
+
